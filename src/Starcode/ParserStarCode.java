0619127b54c9e3db4ea08f860e222bac9157bbe4
Iker Sanmartin <ikersanmartinbarreiro@gmail.com>
2024-10-03 18:29:10 +0200

    First methods for parser

--- src/Starcode/ParserStarCode.java
+++ src/Starcode/ParserStarCode.java
@@ -39,6 +39,81 @@ public class ParserStarCode {
             parseOneDeclaration();
     }
 
+    private void parseOneDeclaration() {
+        switch( currentTerminal.kind ){
+            case STAR:
+                accept(STAR);
+                accept(IDENTIFIER);
+                accept(SEMICOLON);
+                break;
+            case COMMET:
+                accept(COMMET);
+                accept(IDENTIFIER);
+                accept(SEMICOLON);
+                break;
+            case SUPERNOVA:
+                accept(SUPERNOVA);
+                parseIDReturn();
+                accept(IDENTIFIER);
+                accept(LEFTPARAN);
+                parseIDList();
+                accept(RIGHTPARAN);
+                parseSuperNovaBlock();
+                accept(SEMICOLON);
+                break;
+        }
+    }
+
+    private void parseSuperNovaBlock() {
+        accept(LEFTKEY);
+        parseStatements();
+        accept(RETURN);
+        accept(IDENTIFIER);
+        accept(SEMICOLON);
+        accept(RIGHTKEY);
+    }
+
+    private void parseStatements() {
+        while( currentTerminal.kind == ECLIPSE ||
+                currentTerminal.kind == ORBIT ||
+                currentTerminal.kind == IDENTIFIER){
+            parseOneStatement();
+        }
+    }
+
+    private void parseOneStatement() {
+    }
+
+    //We are not allowing different types for parameters in the methods
+    private void parseIDList() {
+        accept(IDENTIFIER);
+        if (currentTerminal.kind == COMMA) {
+            while (currentTerminal.kind == COMMA) {
+                accept(COMMA);
+                accept(IDENTIFIER);
+            }
+        }
+        if (currentTerminal.kind == LEFTBRACKET) {
+                accept(LEFTBRACKET);
+                accept(RIGHTBRACKET);
+                while (currentTerminal.kind == COMMA) {
+                    accept(COMMA);
+                    accept(IDENTIFIER);
+                    accept(LEFTBRACKET);
+                    accept(RIGHTBRACKET);
+            }
+        }
+    }
+
+    private void parseIDReturn() {
+        accept(IDENTIFIER);
+        if (currentTerminal.kind == LEFTBRACKET){
+            accept(LEFTBRACKET);
+            accept(RIGHTBRACKET);
+        }
+    }
+
+
     private void accept( TokenKind expected )
     {
         if( currentTerminal.kind == expected )
