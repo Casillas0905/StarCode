5d1f538691809e454b58e58aa3fa873ade272559
Dan Vyhlidal <dan.vyhlidal@chargedmonkey.com>
2024-10-29 21:07:24 +0100

    refactoring the test drivers

--- src/Starcode/TestDriverScanner.java
+++ src/Starcode/TestDriverScanner.java
@@ -2,24 +2,23 @@ package Starcode;
 
 import javax.swing.*;
 
- 
 public class TestDriverScanner
 {
 	private static final String EXAMPLES_DIR = "C:\\Users\\ikers\\Downloads\\IntLangExamples";
-	
-	
-	public static void main( String args[] )
+
+	public static void main(String args[])
 	{
-		JFileChooser fc = new JFileChooser( EXAMPLES_DIR );
+		JFileChooser fc = new JFileChooser(EXAMPLES_DIR);
 		
-		if( fc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ) {
-			SourceFile in = new SourceFile( fc.getSelectedFile().getAbsolutePath() );
-			Scanner s = new Scanner( in );
+		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+		{
+			SourceFile in = new SourceFile( fc.getSelectedFile().getAbsolutePath());
+			Scanner s = new Scanner(in);
 		
 			Token t = s.scan();
-			while( t.kind != TokenKind.EOT ) {
-				System.out.println( t.kind + " " + t.spelling );
-			
+			while(t.kind != TokenKind.EOT)
+			{
+				System.out.println(t.kind + " " + t.spelling);
 				t = s.scan();
 			}
 		}
