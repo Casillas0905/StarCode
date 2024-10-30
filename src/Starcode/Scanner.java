f760cfcf7bd84adece160fcc41b03c0a5483d17e
Dan Vyhlidal <dan.vyhlidal@chargedmonkey.com>
2024-10-30 16:04:21 +0100

    refactoring scanner

--- src/Starcode/Scanner.java
+++ src/Starcode/Scanner.java
@@ -8,129 +8,113 @@ public class Scanner
 	
 	private char currentChar;
 	private StringBuffer currentSpelling = new StringBuffer();
-	
-	
-	public Scanner( SourceFile source )
+
+	public Scanner(SourceFile source)
 	{
 		this.source = source;
 		
 		currentChar = source.getSource();
 	}
-	
-	
-	private void takeIt()
+
+	public Token scan()
 	{
-		currentSpelling.append( currentChar );
-		currentChar = source.getSource();
+		while(currentChar == '\n' || currentChar == '\r' || currentChar == '\t' || currentChar == ' ')
+			scanSeparator();
+
+		currentSpelling = new StringBuffer("");
+		TokenKind kind = scanToken();
+
+		return new Token( kind, new String( currentSpelling ) );
 	}
-	
-	
-	private boolean isLetter( char c )
-	{
-		return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
-	}
-	
-	
-	private boolean isDigit( char c )
-	{
-		return c >= '0' && c <= '9';
-	}
-	
-	
-	private void scanSeparator()
-	{
-		switch( currentChar ) {
-			case ' ': case '\n': case '\r': case '\t':
-				takeIt();
-				break;
-		}
-	}
-	
-	
+
 	private TokenKind scanToken()
 	{
-		if( isLetter( currentChar ) ) {
+		if(isLetter(currentChar))
+		{
 			takeIt();
-			while( isLetter( currentChar ) || isDigit( currentChar ) )
+			while(isLetter(currentChar) || isDigit(currentChar))
 				takeIt();
-				
-			return IDENTIFIER;
-			
-		} else if( isDigit( currentChar ) ) {
-			takeIt();
-			while( isDigit( currentChar ) )
-				takeIt();
-				
-			return COMETLITERAL;
-			
-		} switch( currentChar ) {
-			case '+': case '-': case '*': case '/': case '%': case '<': case '>':
-				takeIt();
-				return OPERATOR;
 
+			return IDENTIFIER;
+		}
+		else if(isDigit(currentChar))
+		{
+			takeIt();
+			while(isDigit(currentChar))
+				takeIt();
+
+			return COMETLITERAL;
+		}
+
+		switch(currentChar)
+		{
+			case '+': case '-': case '*': case '/':
+			takeIt();
+			return OPERATOR;
 			case ',':
 				takeIt();
 				return COMMA;
-
 			case '=':
 				takeIt();
 				return OPERATOR;
-				
 			case ';':
 				takeIt();
 				return SEMICOLON;
-				
 			case '(':
 				takeIt();
 				return LEFTPARAN;
-				
 			case ')':
 				takeIt();
 				return RIGHTPARAN;
-
 			case '{':
 				takeIt();
 				return LEFTKEY;
-
 			case '\"':
 				takeIt();
 				return QUOTE;
-
 			case '}':
 				takeIt();
 				return RIGHTKEY;
-
 			case '~':
 				takeIt();
 				return TILDE;
-
 			case ']':
 				takeIt();
 				return RIGHTBRACKET;
-
 			case '[':
 				takeIt();
 				return LEFTBRACKET;
-
 			case SourceFile.EOT:
 				return EOT;
-				
 			default:
 				takeIt();
 				return ERROR;
 		}
 	}
-	
-	
-	public Token scan()
+
+	private void scanSeparator()
 	{
-		while( currentChar == '\n' || currentChar == '\r' || currentChar == '\t' ||
-		       currentChar == ' ' )
-			scanSeparator();
-			
-		currentSpelling = new StringBuffer( "" );
-		TokenKind kind = scanToken();
-		
-		return new Token( kind, new String( currentSpelling ) );
+		switch(currentChar)
+		{
+			case ' ': case '\n': case '\r': case '\t':
+			takeIt();
+			break;
+		}
+	}
+
+	private void takeIt()
+	{
+		currentSpelling.append(currentChar);
+		currentChar = source.getSource();
+	}
+
+	private boolean isLetter(char c)
+	{
+		return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+	}
+	
+	private boolean isDigit(char c)
+	{
+		return c >= '0' && c <= '9';
 	}
 }
