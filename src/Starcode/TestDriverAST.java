740213dd8787ea283a110aea60bf026fd535adee
Dan Vyhlidal <dan.vyhlidal@chargedmonkey.com>
2024-10-31 02:19:38 +0100

    adding test driver AST

---
+++ src/Starcode/TestDriverAST.java
@@ -0,0 +1,27 @@
+package Starcode;
+
+
+import Starcode.ast.AST;
+
+import javax.swing.*;
+
+
+public class TestDriverAST
+{
+	private static final String EXAMPLES_DIR = "c:\\usr\\undervisning\\CMC\\IntLang\\examples";
+	
+	
+	public static void main( String args[] )
+	{
+		JFileChooser fc = new JFileChooser( EXAMPLES_DIR );
+		
+		if( fc.showOpenDialog( null ) == fc.APPROVE_OPTION ) {
+			SourceFile in = new SourceFile( fc.getSelectedFile().getAbsolutePath() );
+			Scanner s = new Scanner( in );
+			ParserAST p = new ParserAST( s );
+		
+			AST ast = p.parseProgram();
+			//new ASTViewer( ast );
+		}
+	}
+}
