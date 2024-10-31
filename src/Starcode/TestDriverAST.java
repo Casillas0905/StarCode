2cf77ef49af8d09b8e3c557a199de66eadb8ef84
Dan Vyhlidal <dan.vyhlidal@chargedmonkey.com>
2024-10-31 02:32:05 +0100

    refactoring TestDriverAST

--- src/Starcode/TestDriverAST.java
+++ src/Starcode/TestDriverAST.java
@@ -5,20 +5,19 @@ import Starcode.ast.AST;
 
 import javax.swing.*;
 
-
 public class TestDriverAST
 {
 	private static final String EXAMPLES_DIR = "c:\\usr\\undervisning\\CMC\\IntLang\\examples";
-	
-	
-	public static void main( String args[] )
+
+	public static void main(String args[])
 	{
-		JFileChooser fc = new JFileChooser( EXAMPLES_DIR );
+		JFileChooser fc = new JFileChooser(EXAMPLES_DIR);
 		
-		if( fc.showOpenDialog( null ) == fc.APPROVE_OPTION ) {
-			SourceFile in = new SourceFile( fc.getSelectedFile().getAbsolutePath() );
-			Scanner s = new Scanner( in );
-			ParserAST p = new ParserAST( s );
+		if(fc.showOpenDialog(null) == fc.APPROVE_OPTION)
+		{
+			SourceFile in = new SourceFile(fc.getSelectedFile().getAbsolutePath());
+			Scanner s = new Scanner(in);
+			ParserAST p = new ParserAST(s);
 		
 			AST ast = p.parseProgram();
 			//new ASTViewer( ast );
